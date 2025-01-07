ba761f8808d31f05c05e3e79fbc0498e566d5454
Mostafa Hassanein <me.cssign@gmail.com>
2025-01-07 18:14:35 +0200

    feat(app): implement use case: list port requests

---
+++ src/main/java/com/github/mostafaism1/mobile_number_portability/app/usecase/ListPortRequestsUseCase.java
@@ -0,0 +1,19 @@
+package com.github.mostafaism1.mobile_number_portability.app.usecase;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import com.github.mostafaism1.mobile_number_portability.app.dto.PortRequestDTO;
+import com.github.mostafaism1.mobile_number_portability.repository.PortRequestRepository;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class ListPortRequestsUseCase {
+
+  private final PortRequestRepository portRequestRepository;
+
+  public List<PortRequestDTO> list() {
+    return portRequestRepository.getAll().stream().map(PortRequestDTO::fromModel)
+        .collect(Collectors.toList());
+  }
+
+}
