0beec5357e9fd4b0cab1ae13e9b26c17ff73f923
Mostafa Hassanein <me.cssign@gmail.com>
2025-01-07 09:15:30 +0200

    feat(domain): define PortRequestRepository interface

---
+++ src/main/java/com/github/mostafaism1/mobile_number_portability/repository/PortRequestRepository.java
@@ -0,0 +1,19 @@
+package com.github.mostafaism1.mobile_number_portability.repository;
+
+import java.util.List;
+import java.util.Optional;
+import com.github.mostafaism1.mobile_number_portability.domain.model.PortRequest;
+
+public interface PortRequestRepository {
+
+  public PortRequest create(PortRequest portRequest);
+
+  public Optional<PortRequest> getById(long id);
+
+  public List<PortRequest> getPendingByNumber(String number);
+
+  public List<PortRequest> getAll();
+
+  public PortRequest update(PortRequest portRequest);
+
+}
