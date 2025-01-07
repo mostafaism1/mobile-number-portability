d87b218ce23240870add926a7e39bd28c5a2afa4
Mostafa Hassanein <me.cssign@gmail.com>
2025-01-07 09:15:00 +0200

    feat(domain): define MobileNumberRepository interface

---
+++ src/main/java/com/github/mostafaism1/mobile_number_portability/repository/MobileNumberRepository.java
@@ -0,0 +1,10 @@
+package com.github.mostafaism1.mobile_number_portability.repository;
+
+import java.util.Optional;
+import com.github.mostafaism1.mobile_number_portability.domain.model.MobileNumber;
+
+public interface MobileNumberRepository {
+
+  public Optional<MobileNumber> getMobileNumberByNumber(String number);
+
+}
