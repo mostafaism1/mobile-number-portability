cab1a7f9ebc7f5b5f811e4d5faf4a8b086183885
Mostafa Hassanein <me.cssign@gmail.com>
2025-01-08 04:27:37 +0200

    feat(infrastructure): configure an h2-db and seed with data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO operator (id, name) values (0, 'vodafone'); 
+INSERT INTO operator (id, name) values (1, 'orange');
+
+INSERT INTO mobile_number (id, operator_id, number) values (0, 0, '01012345678');
